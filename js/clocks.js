10b1a8e17410fbc32b7a8b849ff20bcfb1ad6b39
zoeschwyzer <zoe.schwyzer@gmail.com>
2017-09-15 15:36:38 +0200

    pour avoir les chiffres en lettre

--- js/clocks.js
+++ js/clocks.js
@@ -1,7 +1,7 @@
 
 /*
  * Un petit script mesurant l'écoulement du temps
- * 
+ *
  * Source: https://codepen.io/Tcip/pen/BNKjeN
  * Auteur: Mikael Elmblad, tcip.se
 */
@@ -18,10 +18,84 @@ function metronome() {
   // 1: on définit l'heure, avec l'objet Date()
   // voir documentation: https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Objets_globaux/Date
 
-  var d = new Date();
-  
+  var date = new Date();
+
+	var heure = date.getHours();
+	var minute = date.getMinutes();
+	var seconde = date.getSeconds();
+
   // 2: on affiche l'heure dans notre objet "horloge"
 
-  document.getElementById("horloge").innerHTML = d.toLocaleTimeString();
+  document.getElementById("horloge").innerHTML = num2Letters(seconde);
+
+
+
 
 }
+
+function num2Letters(number) {
+
+    if (isNaN(number) || number < 0 || 999 < number) {
+        return 'Veuillez entrer un nombre entier compris entre 0 et 999.';
+    }
+
+    var units2Letters = ['', 'un', 'deux', 'trois', 'quatre', 'cinq', 'six', 'sept', 'huit', 'neuf', 'dix', 'onze', 'douze', 'treize', 'quatorze', 'quinze', 'seize', 'dix-sept', 'dix-huit', 'dix-neuf'],
+        tens2Letters = ['', 'dix', 'vingt', 'trente', 'quarante', 'cinquante', 'soixante', 'soixante', 'quatre-vingt', 'quatre-vingt'];
+
+    var units = number % 10,
+        tens = (number % 100 - units) / 10,
+        hundreds = (number % 1000 - number % 100) / 100;
+
+    var unitsOut, tensOut, hundredsOut;
+
+
+    if (number === 0) {
+
+        return 'zéro';
+
+    } else {
+
+        // Traitement des unités
+
+        unitsOut = (units === 1 && tens > 0 && tens !== 8 ? 'et-' : '') + units2Letters[units];
+
+        // Traitement des dizaines
+
+        if (tens === 1 && units > 0) {
+
+            tensOut = units2Letters[10 + units];
+            unitsOut = '';
+
+        } else if (tens === 7 || tens === 9) {
+
+            tensOut = tens2Letters[tens] + '-' + (tens === 7 && units === 1 ? 'et-' : '') + units2Letters[10 + units];
+            unitsOut = '';
+
+        } else {
+
+            tensOut = tens2Letters[tens];
+
+        }
+
+        tensOut += (units === 0 && tens === 8 ? 's' : '');
+
+        // Traitement des centaines
+
+        hundredsOut = (hundreds > 1 ? units2Letters[hundreds] + '-' : '') + (hundreds > 0 ? 'cent' : '') + (hundreds > 1 && tens == 0 && units == 0 ? 's' : '');
+
+        // Retour du total
+
+        return hundredsOut + (hundredsOut && tensOut ? '-' : '') + tensOut + (hundredsOut && unitsOut || tensOut && unitsOut ? '-' : '') + unitsOut;
+    }
+
+} // fin de la longue fonction
+
+//
+//
+// var userEntry;
+//
+// while (userEntry = prompt('Indiquez le nombre à écrire en toutes lettres (entre 0 et 999) :')) {
+//
+//     alert(num2Letters(parseInt(userEntry, 10)));
+//
+// }
